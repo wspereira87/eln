4b32d1d36b552a48e3da98e3a237557c54040c92
William Pereira <william.pereira@global.ntt>
2024-02-23 10:44:03 -0300

    Arquivos renomeados

---
+++ eln_convergente.py
@@ -0,0 +1,124 @@
+from rich import print
+import ipaddress
+import json
+
+print()
+print("#" * 70)
+print("Gerador de scripts para REDE CONVERGENTE - Eletronorte".center(70))
+print("#" * 70)
+print()
+
+sites_id = {
+   5: "SED",
+   6: "IPZ",
+   7: "COX",
+   8: "TUC",
+   9: "COL",
+   10: "ALT",
+   11: "GUA",
+   12: "JIP",
+   13: "LEC",
+   14: "PVO",
+   15: "SRB",
+   16: "SLU",
+   17: "TPJ"
+}
+site_id = int(input(f"Informe o ID do Site: \n\n{json.dumps(sites_id, indent=4)}\n\n"))
+print()
+vlan_id = int(input(f"Informe a VLAN da Convergente: "))
+print()
+ce_hostname = str.upper(input(f"Informe o hostname do CE: "))
+print()
+pe_intf1 = str.upper(input(f"Informe a interface 1 do PO21 do Agregador (GIX/X/X): "))
+print()
+pe_intf2 = str.upper(input(f"Informe a interface 2 do PO21 do Agregador (GIX/X/X): "))
+print()
+ce_intf1 = str.upper(input(f"Informe a interface 1 do PO21 do CE (GIX/X/X): "))
+print()
+ce_intf2 = str.upper(input(f"Informe a interface 2 do PO21 do CE (GIX/X/X): "))
+print()
+ospf_area = int(input(f"Informe a area OSPF do site (decimal): "))
+print()
+p2p_net = str(input(f"Informe a rede P2P PE-CE (172.27.16.X/30): "))
+print()
+
+ip_lo200 = str(f"172.27.200.{site_id}")
+# Verificar forma de extrair a máscara para colocar na BDI.
+ip_p2p_pe = list(ipaddress.ip_network(p2p_net).hosts())[0]
+ospf_rid = list(ipaddress.ip_network(p2p_net).hosts())[0]
+
+output = f'''interface {pe_intf1}
+ description CONVERGENTE-{ce_hostname}-{ce_intf1} PO21
+ mtu 9202
+ no ip address
+ negotiation auto
+ load-interval 30
+ channel-group 21 mode active
+ service-policy output TO_CORP
+!
+interface {pe_intf2}
+ description CONVERGENTE-{ce_hostname}-{ce_intf2} PO21
+ mtu 9202
+ no ip address
+ negotiation auto
+ load-interval 30
+ channel-group 21 mode active
+ service-policy output TO_CORP
+!
+!
+interface Port-channel21
+ description TRUNK PO21 - REDE CONVERGENTE
+ mtu 9202
+ no ip address
+ negotiation auto
+ load-interval 30
+ shutdown
+  service instance 1 ethernet
+  encapsulation untagged
+  l2protocol peer lacp
+  bridge-domain 1
+ !
+ service instance trunk 2 ethernet
+  description L2 CONTROL(CDP,STP) AND L2 SWITCH
+  encapsulation dot1q {vlan_id}
+  rewrite ingress tag pop 1 symmetric
+  l2protocol peer cdp stp
+  bridge-domain from-encapsulation
+  service-policy input FROM_CORP
+!
+interface BDI{vlan_id}
+ description VRF CONVERGENTE
+ ip vrf forwarding CONVERGENTE
+ ip address {ip_p2p_pe} 255.255.255.252
+ ip ospf 200 area 0.0.0.{ospf_area}
+ shutdown
+!
+!
+interface Loopback200
+ description REDE CONVERGENTE
+ ip vrf forwarding CONVERGENTE
+ ip address {ip_lo200} 255.255.255.255
+!
+!
+router ospf 200 vrf CONVERGENTE
+ router-id {ospf_rid}
+ redistribute bgp 65000
+ passive-interface Loopback200
+ default-information originate always
+!
+router bgp 65000
+ address-family ipv4 vrf CONVERGENTE
+  network {ip_lo200} mask 255.255.255.255
+  redistribute connected
+  redistribute static
+  redistribute ospf 200 match internal external 1 external 2
+ exit-address-family
+'''
+
+print()
+print("-" *70)
+print(f"Configuração Convergente - RAGG{sites_id[site_id]}:".center(70))
+print("-" *70)
+print()
+print(output)
+print()
---
+++ eln_negocios.py
@@ -0,0 +1,125 @@
+from rich import print
+import json
+
+print()
+print("#" * 70)
+print("Gerador de scripts para REDE DE NEGÓCIOS - Eletronorte".center(70))
+print("#" * 70)
+print()
+
+sites_id = {
+   5: "SED",
+   6: "IPZ",
+   7: "COX",
+   8: "TUC",
+   9: "COL",
+   10: "ALT",
+   11: "GUA",
+   12: "JIP",
+   13: "LEC",
+   14: "PVO",
+   15: "SRB",
+   16: "SLU",
+   17: "TPJ"
+}
+site_id = int(input(f"Informe o ID do Site: \n\n{json.dumps(sites_id, indent=4)}\n\n"))
+print()
+#vlan_id = int(input(f"Informe a VLAN da Convergente: "))
+#print()
+ce_hostname = str.upper(input(f"Informe o hostname do CE: "))
+print()
+pe_intf1 = str.upper(input(f"Informe a interface 1 do PO40 do Agregador (TEX/X/X): "))
+print()
+pe_intf2 = str.upper(input(f"Informe a interface 2 do PO40 do Agregador (TEX/X/X): "))
+print()
+ce_intf1 = str.upper(input(f"Informe a interface 1 do PO40 do CE (TEX/X/X): "))
+print()
+ce_intf2 = str.upper(input(f"Informe a interface 2 do PO40 do CE (TEX/X/X): "))
+print()
+intf_sw = "PO40"
+print()
+
+output1 = f'''interface {pe_intf1}
+ description NEGOCIOS-{ce_hostname}-{ce_intf1} PO40
+ mtu 9202
+ no ip address
+ load-interval 30
+ channel-group 40 mode active
+!
+interface {pe_intf2}
+ description NEGOCIOS-{ce_hostname}-{ce_intf2} PO40
+ mtu 9202
+ no ip address
+ load-interval 30
+ channel-group 40 mode active
+!
+!
+interface {intf_sw}
+ description NEGOCIOS {ce_hostname}-PO40
+ mtu 9202
+ no ip address
+ load-interval 30
+ shutdown
+  service instance 1 ethernet
+  encapsulation untagged
+  l2protocol peer lacp
+  bridge-domain 1
+ !
+ service instance 800 ethernet
+  encapsulation dot1q 800
+  rewrite ingress tag pop 1 symmetric
+  service-policy input FROM_SCM
+ !
+!
+interface Pseudowire 800
+ description PW_TESTE_NEGOCIOS - {sites_id[site_id]}_SED
+ encapsulation mpls
+ control-word include
+ load-balance flow ethernet src-dst-mac
+ load-balance flow-label both
+ neighbor 172.27.254.5 800{site_id}
+!
+l2vpn
+ logging pseudowire status
+ logging vc-state
+!
+l2vpn xconnect context TESTE_NEGOCIOS
+  member {intf_sw} service-instance 800
+  member Pseudowire 800
+!
+'''
+
+output2 = f'''
+interface Pseudowire 800{site_id}
+ description PW_TESTE_NEGOCIOS - SED_{sites_id[site_id]}
+ encapsulation mpls
+ control-word include
+ load-balance flow ethernet src-dst-mac
+ load-balance flow-label both
+ neighbor 172.27.254.{site_id} 800{site_id}
+!
+!
+!#PRIMEIRO REMOVER SERVICE-INSTANCE DO XCONNECT DA MIGRAÇÃO ANTERIOR:
+!
+l2vpn xconnect context <XCONN_CONTEXT>
+ no member PO40 service-instance 800
+!
+l2vpn xconnect context TESTE_NEGOCIOS_{sites_id[site_id]}
+  member PO40 service-instance 800
+  member Pseudowire 800{site_id}
+!
+'''
+
+print()
+print("-" *70)
+print(f"Configuração Rede de Negócios - RAGG{sites_id[site_id]}:".center(70))
+print("-" *70)
+print()
+print(output1)
+print()
+print("-" *70)
+print(f"Configuração Rede de Negócios - RAGGSED:".center(70))
+print("-" *70)
+print()
+print(output2)
+print()
